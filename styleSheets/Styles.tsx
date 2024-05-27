f4579329723f3bae6085e9899141d7f82a7a5f2c
Wzh <woozongghua@gmail.com>
2024-05-27 18:00:21 +0800

    Implemented User Authentication and Navigating between Login Page and Home Page

--- styleSheets/Styles.tsx
+++ styleSheets/Styles.tsx
@@ -6,5 +6,15 @@ export const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  textInput: {
+    marginVertical: 4,
+    height: 50,
+    width: 300,
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 10,
+    backgroundColor: '#fff',
   },
 });
