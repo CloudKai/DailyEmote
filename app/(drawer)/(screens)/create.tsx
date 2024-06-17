fd51b253610959ea41b3199bb952ffb6910c5513
Wzh <woozongghua@gmail.com>
2024-06-17 14:27:21 +0800

    merged features successfully, calendar that displays entries for each day and add entry feature added. Also attempting to standardise color palette for UI.

--- app/(drawer)/(screens)/create.tsx
+++ app/(drawer)/(screens)/create.tsx
@@ -1,12 +1,141 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, Pressable, SafeAreaView } from 'react-native'
+import DateTimePicker, { DateTimePickerEvent } from '@react-native-community/datetimepicker';
+import React, { useState } from 'react'
+import { entryData, homeStyles } from './home';
+import { router } from 'expo-router';
+import { addDoc, collection } from 'firebase/firestore';
+import { FIREBASE_DB } from '../../../FireBaseConfig';
+import { colors } from '../../../styleSheets/Styles';
 
 const create = () => {
+  const [title, setTitle] = useState("");
+  const [textEntry, setTextEntry] = useState("");
+  const [date, setDate] = useState(new Date());
+  const [dateModal, setDateModal] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [entries, setEntries] = useState([] as entryData[]);
+
+  const addEntry = async () => {
+    try {
+      const entriesRef = collection(FIREBASE_DB, "entries");
+      const { year, month, day } = formatDate(date);
+      const document = await addDoc(entriesRef, {
+        title: title,
+        isHappy: false,
+        year: year,
+        month: month,
+        day: day,
+        textEntry: textEntry,
+      });
+      console.log("Document written with ID: ", document.id);
+      setTitle("");
+      setTextEntry("");
+      router.back();
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
+
+  const onDateChange = (event: DateTimePickerEvent, selectedDate?: Date) => {
+    const currentDate = selectedDate || date;
+    setDateModal(false);
+    setDate(currentDate);
+    console.log(currentDate.toDateString().split(" "));
+  };  
+
+  const formatDate = (date: Date) => {
+    const year: string = date.getFullYear().toString();
+    const month: string = (date.getMonth() + 1) < 10 
+      ? "0" + (date.getMonth() + 1) 
+      : (date.getMonth() + 1).toString();
+    const day: string = (date.getDate()) < 10 
+      ? "0" + (date.getDate()) 
+      : (date.getDate()).toString();
+    return { year, month, day };
+  }
+
   return (
-    <View className = "flex-1 items-center justify-center bg-primary">
-      <Text style={{color: 'white'}}> Create Post Here! </Text>
+    <View style = {{
+      flex: 1,
+      backgroundColor: '#161622',
+    }}>
+      <SafeAreaView style={[
+        {
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          margin: 30,
+          height: "100%"
+        }]}>
+        <Text style={[
+          homeStyles.headingText, {
+            color: colors.primary, //Color: White
+            marginVertical: 20,
+          }]}>
+          New Entry
+        </Text>
+        <TextInput 
+          style={[homeStyles.inputBox, {}]} 
+          placeholder="Title" 
+          value={title}
+          onChangeText={(text) => setTitle(text)}
+          multiline={true}
+          numberOfLines={1}
+        />
+        {/* Date Select */}
+        {dateModal && (
+          <DateTimePicker
+            mode="date"
+            display="spinner"
+            value={date}
+            onChange={onDateChange}
+          />
+        )}
+        <Pressable 
+          style={{width: "100%",}}
+          onPress={() => {
+            setDateModal(true);
+            console.log("Opened date modal")
+          }}
+        >
+          <TextInput 
+            style={[homeStyles.inputBox, {color: colors.tertiary,}]}
+            editable={false}
+          >
+            Date: {formatDate(date).year}-{formatDate(date).month}-{formatDate(date).day}
+          </TextInput>
+        </Pressable>
+        <TextInput 
+          style={[homeStyles.inputBox, {height: "40%", }]} 
+          placeholder="Enter new entry" 
+          value={textEntry}
+          onChangeText={(text) => setTextEntry(text)}
+          multiline={true}
+          numberOfLines={5}
+        />
+        <View style={{}}>
+          <Pressable 
+            style={[
+              homeStyles.button, {
+                backgroundColor: colors.button //Color: Light Blue
+              }]}
+            onPress={addEntry}
+          >
+            <Text style={homeStyles.text}>Add Entry</Text>
+          </Pressable>
+          <Pressable
+            style={[
+              homeStyles.button, {
+                backgroundColor: colors.button //Color: Light Blue
+              }]}
+            onPress={() => router.back()}
+          >
+            <Text style={homeStyles.text}>Back</Text>
+          </Pressable>
+        </View>
+      </SafeAreaView>
     </View>
   )
 }
 
-export default create
+export default create;
--- styleSheets/Styles.tsx
+++ styleSheets/Styles.tsx
@@ -1,16 +1,28 @@
 import { StyleSheet } from 'react-native'
 
+export const colors = {
+  primary: '#fff', // Color: White
+  secondary: '#FAFAFA', // Color: White Smoke
+  tertiary: '#0A0F44', // Color: Dark Navy
+  accent: '#1976D2', //Color: Sky Blue
+  background: '#161622', //Color: Dark Blue
+  secondaryBackground: '#212121', //Color: Dark Gray
+  tertiaryBackground: '#303030', // Color: Charcoal
+  contrastBackground: '#CFD8DC', //Color: Light Gray
+  button: "#6082B6", //Color: Navy Blue
+  border: '#2196F3', //Color: Light Blue
+  shadow: '#000', //Color: Black
+  disabled: "#A9A9A9", //Color: Gray 
+}
+
 export const styles = StyleSheet.create({
-  
   container: {
     flex: 1,
     backgroundColor: '#0C0C1E',
     alignItems: 'center',
     justifyContent: 'center',
     padding: 0,
-    
   },
-
   textInput: {
     marginVertical: 4,
     height: 50,
@@ -20,7 +32,6 @@ export const styles = StyleSheet.create({
     padding: 10,
     backgroundColor: '#fff',
   },
-
   button: {
     borderRadius: 20,
     padding: 10,
