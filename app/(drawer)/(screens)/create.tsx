6a596b865e8b3130908388e731b2619f1921c8d5
Cloudkai <kaiming_01@live.com>
2024-07-23 21:03:04 +0800

    Added Mood and ensure Date doesnt go past the current date

--- app/(drawer)/(screens)/create.tsx
+++ app/(drawer)/(screens)/create.tsx
@@ -1,17 +1,16 @@
-import { View, Text, TextInput, Pressable, SafeAreaView, StyleSheet, Alert } from 'react-native'
-import DateTimePicker, { DateTimePickerEvent } from '@react-native-community/datetimepicker';
-import React, { useState } from 'react'
+import { View, Text, TextInput, Pressable, SafeAreaView, StyleSheet, Alert, TouchableOpacity } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { router } from 'expo-router';
-import { addDoc, collection } from 'firebase/firestore';
-import { FIREBASE_DB } from '../../../FireBaseConfig';
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 import { colors, styles } from '../../../styleSheets/Styles';
 import { addEntry, formatDate } from '../../../utils/FireBaseHandler';
 import ConfirmButton from '../../../components/ConfirmButton';
 import DateInput from '../../../components/entry/DateInput';
 import DescriptionInput from '../../../components/entry/DescriptionInput';
 import TitleInput from '../../../components/entry/TitleInput';
-import { BackButton } from '../../../components/BackButton';
 import HeaderComponent from '../../../components/HeaderComponent';
+import { red } from 'react-native-reanimated/lib/typescript/reanimated2/Colors';
+import { color } from '@rneui/base';
 
 /**
  * Create entry screen of the app
@@ -19,10 +18,12 @@ import HeaderComponent from '../../../components/HeaderComponent';
  */
 export default function create() {
 
+  const currDate = formatDate(new Date());
   const [title, setTitle] = useState("");
   const [textEntry, setTextEntry] = useState("");
-  const [dateString, setDateString] = useState(formatDate(new Date())); //Format: "YYYY-MM-DD"
-
+  const [dateString, setDateString] = useState(currDate); //Format: "YYYY-MM-DD"
+  const [mood, setMood] = useState("Happy");
+  
   /**
    * Function to handle when the add entry button is pressed
    */
@@ -35,6 +36,10 @@ export default function create() {
       addEntry(title, dateString, textEntry);
     }
   }
+
+  const triggerMood = (newMood: React.SetStateAction<string>) => {
+    setMood(newMood);
+  };
 
   const goBack = () => {
     router.back();
@@ -49,27 +54,78 @@ export default function create() {
       justifyContent: "flex-start",
     }}>
       <View style={addEntryStyles.headerContainer}>
-        <HeaderComponent title="Create Entry" goBack={goBack}/>
+        <HeaderComponent title="Create Entry" goBack={goBack} />
       </View>
 
-      <View style={{padding: 10, alignItems: "center"}}>
+      <View style={{ padding: 10, alignItems: "center" }}>
 
         <View style={addEntryStyles.inputContainer}>
-          <DateInput text={dateString} setText={setDateString}/>
+          <DateInput text={dateString > currDate ? currDate : dateString} setText={setDateString} />
         </View>
 
         <View style={addEntryStyles.inputContainer}>
-          <TitleInput text={title} setText={setTitle}/>
+          <TitleInput text={title} setText={setTitle} />
+        </View>
+
+        <Text style={{
+          fontSize: 20,
+          color: 'white',
+          paddingTop: 5,
+          alignSelf: 'center'
+        }}>
+          Mood:
+        </Text>
+        <View style={{
+          flexDirection: 'row',
+          padding: 20,
+          justifyContent: 'space-between',
+          alignContent: 'space-evenly',
+        }}>
+          <TouchableOpacity
+            onPress={() => triggerMood("Sad")}
+            style={{
+              paddingHorizontal: 30,
+            }}>
+            <MaterialCommunityIcons
+              name="emoticon-sad-outline"
+              size={60}
+              color={mood === "Sad" ? "red" : "gray"}
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => triggerMood("Neutral")}
+            style={{
+              paddingHorizontal: 30,
+            }}>
+            <MaterialCommunityIcons
+              name="emoticon-neutral-outline"
+              size={60}
+              color={mood === "Neutral" ? "yellow" : "gray"}
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => triggerMood("Happy")}
+            style={{
+              paddingHorizontal: 30,
+            }}>
+            <MaterialCommunityIcons
+              name="emoticon-happy-outline"
+              size={60}
+              color={mood === "Happy" ? "green" : "gray"}
+            />
+          </TouchableOpacity>
         </View>
 
         <View style={addEntryStyles.inputContainer}>
-          <DescriptionInput text={textEntry} setText={setTextEntry}/>
+          <DescriptionInput text={textEntry} setText={setTextEntry} />
         </View>
 
       </View>
 
       <View style={addEntryStyles.buttonContainer}>
-        <ConfirmButton handlePress={handleAddEntry} title="Add Entry"/>
+        <ConfirmButton handlePress={handleAddEntry} title="Add Entry" />
       </View>
 
     </SafeAreaView>
@@ -88,7 +144,7 @@ const addEntryStyles = StyleSheet.create({
   inputContainer: {
     justifyContent: "center",
     alignItems: "center",
-    padding: 10,
+    padding: 7,
     flexDirection: "row",
     borderColor: 'red',
     borderWidth: 0,
@@ -96,7 +152,6 @@ const addEntryStyles = StyleSheet.create({
   buttonContainer: {
     justifyContent: "center",
     alignItems: "center",
-    padding: 1,
     width: "100%",
   },
 });
